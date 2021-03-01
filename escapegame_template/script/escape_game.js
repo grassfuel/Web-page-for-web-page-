c2c2fc4faea0cd290f79a76af49b03f5a5cba64f
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-01 23:49:22 +0900

    軽微な変更
    元の描画のさせ方をクラスに置き換え.

--- escapegame_template/script/escape_game.js
+++ escapegame_template/script/escape_game.js
@@ -36,16 +36,20 @@ class Click_object extends Escape_game_object{
 }
 //視点変更用ボタン
 class View_change extends Click_object{
-    constructor(x,y,hit_width,hit_height,disp,skip_page){
-        super(x,y,hit_width,hit_height,disp,null)
+    constructor(x,y,hit_width,hit_height,skip_page){
+        super(x,y,hit_width,hit_height,null,null)
         this.skip_page = skip_page;
     }
-    draw(ctx){
-        ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
+    draw(ctx,cursor){
+        if(cursor.clientX > this.x && cursor.clientX < this.x+this.hit_width && cursor.clientY > this.y && cursor.clientY < this.y+this.hit_height){
+            ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
+        }
     }
-    click(ctx){
+    click(ctx,cursor){
         ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
-        return this.skip_page;
+        if(cursor.clientX > this.x && cursor.clientX < this.x+this.hit_width && cursor.clientY > this.y && cursor.clientY < this.y+this.hit_height){
+            return this.skip_page;
+        }
     }
 }
 
@@ -57,6 +61,8 @@ function key_get(){
     let i=0;
     let page = [new Room("./img/front.png"),new Room("./img/right.png"),new Room("./img/back.png"),new Room("./img/left.png")];
     let item = new Click_object(10,10,3,"./img/item.png");
+    let right_view = new View_change(canvas.getBoundingClientRect().width*0.67,0,canvas.getBoundingClientRect().width*0.05,canvas.getBoundingClientRect().height,1);
+    let left_view = new View_change(0,0,canvas.getBoundingClientRect().width*0.05,canvas.getBoundingClientRect().height,-1);
     let pagecount = 0;
     function get_down_key(e){
         ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -67,6 +73,12 @@ function key_get(){
     }
     function get_mouse_xy(e){
         i+=1;
+        if(i > 3){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            right_view.draw(ctx,e);
+            left_view.draw(ctx,e);
+        }
+        /*
         if(i > 3){
             i=0;
             if(e.clientX > canvas.getBoundingClientRect().width*0.95){
@@ -79,8 +91,12 @@ function key_get(){
                 }
             }
         }
+        */
     }
     function get_click(e){
+        pagecount += right_view.click(ctx,e);
+        pagecount += left_view.click(ctx,e);
+        /*
         if(e.clientX > canvas.getBoundingClientRect().width*0.95){
             pagecount+=1;
             if(pagecount > 3){pagecount = 0}
@@ -91,6 +107,7 @@ function key_get(){
                 if(pagecount < 0){pagecount = 3}
             }
         }
+        */
         console.log("page:"+ pagecount);
         page[pagecount].draw(ctx);
         item.draw(ctx,pagecount);
