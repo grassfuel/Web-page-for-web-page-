9002a052b7423be442f4a1677b40c2a41e2ba3e7
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-05 23:50:36 +0900

    サイドバー開閉ボタンと，サイドバー本体で切り分ける
    class を別々にしサイドバーの開閉ボタンと，サイドバー本体を明確に分けた. あとは，外側に出すボタンを取り付けるだけ.

--- サイドメニュー_canvas/script/side_menu.js
+++ サイドメニュー_canvas/script/side_menu.js
@@ -39,35 +39,48 @@ class Side_bar_b extends Click_object{
         ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
     }
     click(ctx,canvas,cursor,side_bar){
-        console.log(this.x +":"+ canvas.x*0.8)
-        if(cursor.x > this.x && cursor.x < this.x+this.hit_width && cursor.y > this.y && cursor.y < this.y+this.hit_height){
+        console.log(cursor.layerX +":"+ cursor.layerY);
+        if(cursor.layerX > this.x && cursor.layerX < this.x+this.hit_width && cursor.layerY > this.y && cursor.layerY < this.y+this.hit_height){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
             if(this.x <= canvas.width*0.8){
                 this.x = canvas.width - canvas.width*0.05
+                side_bar.x = this.x + this.hit_width;
                 console.log("ちちみ");
             }else{
                 this.x = canvas.width*0.8
+                side_bar.x = this.x + this.hit_width;
                 console.log("のび");
             }
-            ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
+            this.draw(ctx);
+            side_bar.draw(ctx);
+
             return 1;
         }else{
             return 0;
         }
     }
 }
-
+class Side_bar_m extends Click_object{
+    constructor(x,y,hit_width,hit_height,disp,src){
+        super(x,y,hit_width,hit_height,disp,src);
+    }
+    draw(ctx){
+        ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
+    }
+}
 
 function key_get(){
     let canvas = document.getElementById("canvas");
     canvas.width = canvas.getBoundingClientRect().width;
     canvas.height = canvas.getBoundingClientRect().height;
     let ctx = canvas.getContext("2d");
-    let side_barbutton = new Side_bar_b(canvas.width*0.8,0,canvas.width*0.2,canvas.height,0);
+    let side_barmain = new Side_bar_m(canvas.width*0.8,0,canvas.width*0.2,canvas.height,0);
+    let side_barbutton = new Side_bar_b(canvas.width*0.75,0,canvas.width*0.05,50,0);
     side_barbutton.draw(ctx);
+    side_barmain.draw(ctx);
 
     function get_click(e){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        console.log(side_barbutton.click(ctx,canvas,e));
+        console.log(side_barbutton.click(ctx,canvas,e,side_barmain));
     }
     canvas.setAttribute("tabindex",0);
     canvas.addEventListener("click",get_click,false);
