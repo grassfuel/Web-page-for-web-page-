cacb108cbe95ce65d3e23cbbc7f0372271096810
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-07 23:44:05 +0900

    Add files via upload

--- サイドメニュー_canvas/script/side_menu.js
+++ サイドメニュー_canvas/script/side_menu.js
@@ -49,21 +49,25 @@ class Side_bar_b extends Click_object{
     }
     click(ctx,canvas,cursor,side_bar,hide_b){
         console.log(cursor.layerX +":"+ cursor.layerY);
-        if(this.hit_box(cursor) == 1){
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            if(this.x <= canvas.width*0.8){
-                this.x = canvas.width - canvas.width*0.05
-                side_bar.x = this.x + this.hit_width;
-                console.log("ちちみ");
+        if(hide_b.hide == 0){
+            if(this.hit_box(cursor) == 1){
+                ctx.clearRect(0,0,canvas.width,canvas.height);
+                if(this.x <= canvas.width*0.8){
+                    this.x = canvas.width - canvas.width*0.05
+                    side_bar.x = this.x + this.hit_width;
+                    console.log("ちちみ");
+                }else{
+                    this.x = canvas.width*0.8
+                    side_bar.x = this.x + this.hit_width;
+                    console.log("のび");
+                }
+                this.draw(ctx);
+                side_bar.draw(ctx);
+                hide_b.draw(ctx);
+                return 1;
             }else{
-                this.x = canvas.width*0.8
-                side_bar.x = this.x + this.hit_width;
-                console.log("のび");
+                return 0;
             }
-            this.draw(ctx);
-            side_bar.draw(ctx);
-            hide_b.draw(ctx);
-            return 1;
         }else{
             return 0;
         }
@@ -80,12 +84,17 @@ class Side_bar_m extends Click_object{
 class hide_button extends Click_object{
     constructor(x,y,hit_width,hit_height,disp,src){
         super(x,y,hit_width,hit_height,disp,src);
+        this.hide = 0;
     }
     draw(ctx){
         ctx.fillRect(this.x,this.y,this.hit_width,this.hit_height);
     }
     click(ctx,cavas,cursor){
-
+        if(this.hide == 0){
+            this.hide = 1;
+        }else{
+            this.hide = 0;
+        }
     }
 }
 function key_get(){
@@ -95,14 +104,16 @@ function key_get(){
     let ctx = canvas.getContext("2d");
     let side_barmain = new Side_bar_m(canvas.width*0.8,0,canvas.width*0.2,canvas.height,0);
     let side_barbutton = new Side_bar_b(canvas.width*0.75,0,canvas.width*0.05,50,0);
+    let side_bar_hide = new hide_button(canvas.width*0.75,70,canvas.width*0.05,50,0)
     side_barbutton.draw(ctx);
     side_barmain.draw(ctx);
 
     function get_click(e){
-        console.log(side_barbutton.click(ctx,canvas,e,side_barmain));
+        console.log(side_barbutton.click(ctx,canvas,e,side_barmain,side_bar_hide));
     }
     canvas.setAttribute("tabindex",0);
     canvas.addEventListener("click",get_click,false);
 }
 
 key_get();
+
