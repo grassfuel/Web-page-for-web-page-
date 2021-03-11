631dad0471c6e4242c54ac8589d71ee2e4a4845a
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-12 00:34:59 +0900

    ルーレットを停止可能に変更.
    変数 count 1~5 をフラグ代わりに使うことで停止を可能にした，他の方法が思いついた場合は変更したい.

--- ルーレット/script/script.js
+++ ルーレット/script/script.js
@@ -1,3 +1,4 @@
+//　ルーレットの中身
 let style = ["1","2","3","4"];
 let set =[
                 ["a","b","c"],
@@ -13,16 +14,15 @@ let subset =[
                 ["a","b","c"],
                 ["a","b","c"]
             ];
+// ルーレット停止のフラグ，もう少し良い方法を模索したい
+let count1= true;
+let count2= true;
+let count3= true;
+let count4= true;
+let count5= true;
 
-let count1= 0;
-let count2= 0;
-let count3= 0;
-let count4= 0;
 
-function switchon(){
-    return 1;
-}
-document.getElementById("wepon1").onclick = switchon();
+// ルーレット関数，　アニメーションフレームでループする.
 function loop(){
     function styleloop(item,asset,line){
         item.innerHTML = asset[parseInt(Math.random()*asset.length)];
@@ -30,14 +30,11 @@ function loop(){
     function weponloop(item,asset,line){
         item.innerHTML = asset[1][parseInt(Math.random()*asset[0].length)];
     }
-
-
-    console.log( document.getElementById("wepon1").onclick);
-    styleloop(document.getElementById("style"),style,0);
-    weponloop(document.getElementById("wepon1"),set,0);
-    weponloop(document.getElementById("wepon2"),set,0);
-    weponloop(document.getElementById("wepon3"),set,0);
-    weponloop(document.getElementById("wepon4"),set,0);
+    if(count1){styleloop(document.getElementById("style"),style,0);}
+    if(count2){weponloop(document.getElementById("wepon1"),set,0);}
+    if(count3){weponloop(document.getElementById("wepon2"),set,0);}
+    if(count4){weponloop(document.getElementById("wepon3"),set,0);}
+    if(count5){weponloop(document.getElementById("wepon4"),set,0);}
     requestAnimationFrame(loop);
 }
 loop()
