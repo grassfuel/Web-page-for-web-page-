32ba84f254dfa262be1c14ff69dca2f568da842c
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-12 02:22:50 +0900

    ui を若干見れる状態に
    cssを加筆して若干デザインの変更

--- ルーレット/script/script.js
+++ ルーレット/script/script.js
@@ -2,10 +2,10 @@
 let style = ["1","2","3","4"];
 let set =[
                 ["a","b","c"],
-                ["a","b","c"],
-                ["a","b","c"],
-                ["a","b","c"],
-                ["a","b","c"]
+                ["d","e","f"],
+                ["g","h","i"],
+                ["j","k","l"],
+                ["m","n","o"]
             ];
 let subset =[
                 ["a","b","c"],
@@ -20,21 +20,23 @@ let count2= true;
 let count3= true;
 let count4= true;
 let count5= true;
-
+let array_line = 0;
 
 // ルーレット関数，　アニメーションフレームでループする.
 function loop(){
-    function styleloop(item,asset,line){
-        item.innerHTML = asset[parseInt(Math.random()*asset.length)];
+    function styleloop(item,asset){
+        let i = parseInt(Math.random()*asset.length);
+        item.innerHTML = asset[i];
+        return i;
     }
     function weponloop(item,asset,line){
-        item.innerHTML = asset[1][parseInt(Math.random()*asset[0].length)];
+        item.innerHTML = asset[line][parseInt(Math.random()*asset[line].length)];
     }
-    if(count1){styleloop(document.getElementById("style"),style,0);}
-    if(count2){weponloop(document.getElementById("wepon1"),set,0);}
-    if(count3){weponloop(document.getElementById("wepon2"),set,0);}
-    if(count4){weponloop(document.getElementById("wepon3"),set,0);}
-    if(count5){weponloop(document.getElementById("wepon4"),set,0);}
+    if(count1){array_line = styleloop(document.getElementById("style"),style);}
+    if(count2){weponloop(document.getElementById("wepon1"),set,array_line);}
+    if(count3){weponloop(document.getElementById("wepon2"),set,array_line);}
+    if(count4){weponloop(document.getElementById("wepon3"),set,array_line);}
+    if(count5){weponloop(document.getElementById("wepon4"),set,array_line);}
     requestAnimationFrame(loop);
 }
 loop()
