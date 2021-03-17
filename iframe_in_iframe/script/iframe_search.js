7656b8f1e733f2734d3b4986718084f344372f85
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-17 21:42:24 +0900

    若干更新 idの記述
    iframe内部のタグに対する書き換え方法がまだわからないが一応更新

--- iframe_in_iframe/script/iframe_search.js
+++ iframe_in_iframe/script/iframe_search.js
@@ -1,5 +1,17 @@
+function loop(){
+    let i=0;
+    for(i=0;i<50;i+=1){
+        document.getElementById("loo"+i).innerHTML = "<iframe id=\"loo"+i+"\"src=\"lv3\"></iframe>";
+    }
+}
+
+
 function search(){
     console.log(document.getElementById("lv1"));
-    document.getElementById("out_put").innerHTML ="a";
+    document.getElementById("out_put_1").innerHTML ="a";
+    document.getElementById("out_put_2").innerHTML ="a";
+    document.getElementById("out_put_3").innerHTML ="a";
 }
-search();
+window.onload = function(){
+    search();
+}
