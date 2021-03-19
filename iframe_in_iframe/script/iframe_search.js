cff0671cce6d2fc95f4dfc056ed1e0aee8042ae0
grassfuel <50146395+grassfuel@users.noreply.github.com>
2021-03-19 22:03:41 +0900

    Add files via upload

--- iframe_in_iframe/script/iframe_search.js
+++ iframe_in_iframe/script/iframe_search.js
@@ -7,7 +7,21 @@ function loop(){
 
 
 function search(){
+    let i = document.getElementById("lv1");
     console.log(document.getElementById("lv1"));
+    console.log(document.getElementById("lv1").innerHTML);
+    console.log(document.getElementById("lv1").click);
+    console.log(document.getElementById("lv1").classList);
+    console.log(document.getElementById("lv1").getAttribute);
+    console.log(document.getElementById("lv1").querySelector);
+    console.log(document.getElementById("lv1").accessKey);
+    console.log(document.getElementById("lv1").accessKeyLabel);
+    console.log(document.getElementById("lv1").assignedSlot);
+    console.log(document.getElementById("lv1").attributes);
+    console.log(document.getElementById("lv1").autocapitalize);
+    console.log(document.getElementById("lv1").dir);
+    console.log(document.getElementById("lv1").nodeName);
+    console.log(document);
     document.getElementById("out_put_1").innerHTML ="a";
     document.getElementById("out_put_2").innerHTML ="a";
     document.getElementById("out_put_3").innerHTML ="a";
